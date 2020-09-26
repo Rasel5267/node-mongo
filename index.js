221c279ac4b418c00a190c79439a85e51488ce9b
Rasel5267 <mahadiul09@gmail.com>
2020-09-26 19:38:05 +0600

    first stape done

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const express = require('express')
 const app = express();
 
 app.get('/', (req, res) =>{
-    res.send('Thank you very much');
+    res.send('node project');
   })
-
+app.get('/fruits/banana', (req, res) =>{
+  res.send({fruits:'banana', quantity:'1000', price:'10000'});
+})
 app.listen(3000, () => console.log('listening on port 3000'));
